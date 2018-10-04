fe385c5c8241b7cb3ff1874e6272b6a3cb138e6f
Anna Moeller <anna.moeller@umontana.edu>
2018-10-04 09:55:37 -0600

    Redid parts a and b in matrix (plane crash prob)

--- Probability Theory/Homework 4.R
+++ Probability Theory/Homework 4.R
@@ -4,40 +4,28 @@
 
   # Prior probabilities that the plane went down in each region
   # Locations are: 1) Forest, 2) Mountains, 3) Plains
-  pr_region <- matrix(c(.5, .3, .2) )
-  pr_F <- 0.5
-  pr_M <- 0.3
-  pr_P <- 0.2
+  pr_region <- matrix(c(.5, .3, .2, rep(NA, 15)) , nrow = 3)
   
   # Conditional probabilities of a successful search given the plane is there
-  pr_SFgF <- 0.3
-  pr_SMgM <- 0.5
-  pr_SPgP <- 0.8
+  pr_success_g_its_there <- matrix(c(.3, .5, .8, rep(NA, 15)), nrow = 3)
   
   # probability of finding plane on first search
-  pr_SF <- pr_SFgF * pr_F
-  pr_SM <- pr_SMgM * pr_M
-  pr_SP <- pr_SPgP * pr_P
+  pr_success <- pr_success_g_its_there * pr_region
   
   # Which place do we search? 
-  search <- which(c(pr_SF, pr_SM, pr_SP) == max(pr_SF, pr_SM, pr_SP))
+  search <- which(pr_success[, i] == max(pr_success[, i]))
   
   # Probability that the search failed
-  pr_failed <- 1 - c(pr_SF, pr_SM, pr_SP)[search]
+  pr_failed <- 1 - pr_success[search, i]
   
   # Posterior probability of being in region given failed search
-  if(search == 1){
-    pr_F <- pr_F * (1 - pr_SFgF) * pr_F/ pr_failed
-  } else{
-    pr_F <- 1 * pr_F/pr_failed
+  for(j in 1:3){
+    if(j == search){
+      pr_success[j, i + 1] <- pr_region[j, i] * (1 - pr_success_g_its_there[j, i]) * 
+        pr_region[j, i] / pr_failed
+    } else {
+      pr_success[j, i+1] <- pr_region[j, i]/pr_failed
+    }
   }
-  if(search == 2){
-    pr_M <- pr_M * (1 - pr_SMgM) * pr_M/ pr_failed
-  } else {
-    pr_M <- 1 * pr_M/pr_failed
-  }
-  if(search == 3){
-    pr_P <- pr_P * (1 - pr_SPgP) * pr_P/ pr_failed
-  } else {
-    pr_P <- 1 * pr_P/pr_failed
-  }
+
+ 

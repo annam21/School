5ecc1211b2806fef8275525d29ec029041527b5a
Anna Moeller <anna.moeller@umontana.edu>
2018-11-09 18:03:49 -0700

    Finished homework 7

--- Probability Theory/Homework 7.R
+++ Probability Theory/Homework 7.R
@@ -5,12 +5,19 @@
   # Additional problems
 
   # Number 1
-  
+  n <- 1000000
+  X <- rexp(n, 1)
+  Y <- rexp(n, 1)
+  U <- X/(X + Y)
+  hist(U, freq = F)
+  abline(h = 1, col = "red")
 
   # Number 3
   n <- 1000000
   X <- runif(n)
   Y <- runif(n)
   Z <- X/(X-Y)
-  hist(Z, breaks = 1000000, xlim = c(-50, 50), freq = F)
+  hist(Z, breaks = 1000000, xlim = c(-30, 30), freq = F)
+  curve(1/(2*x^2), add = T, col = "red")
+  
   
